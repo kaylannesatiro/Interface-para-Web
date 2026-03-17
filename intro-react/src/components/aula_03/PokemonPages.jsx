93c1c182434c5790535d429e9430365ffce13b1e
Kaylanne <kaylannesatiro@icloud.com>
2026-03-17 16:51:39 -0300

    Aula 03: Pokemon Pages

--- intro-react/src/components/aula_03/PokemonPages.jsx
+++ intro-react/src/components/aula_03/PokemonPages.jsx
@@ -1,9 +1,45 @@
+import { use, useState } from "react";
+
 const PokemonPages = () => {
+
+    const spriteURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+    const [id, setId] = useState(100)
+
+    const addId = () => {
+        setId((x) => x + 1)
+    }
+
+    const subId = () => {
+        setId((x) => x - 1)
+    }
+
     return (
         <>
-            <div>
-                <h1>Pokemon Pages</h1>
-            </div>
+            <nav className="navbar bg-primary" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Pokemon Pages</a>
+                </div>
+            </nav>
+
+            <main className="container">
+                <h2>Navegador de Pokemons</h2>
+
+                <div className="card" style={{width: 300}}>
+                    <img src={spriteURL + id + ".png"} className="card-img-top" alt="Pokemon" style={{width: 250}}/>
+
+                    <div className="card-body">
+                        <h5 className="card-title">
+                            Nome do Pokemon
+                        </h5>
+                        <p className="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et id exercitationem voluptate at dolores dolorum molestiae omnis dolor quae quaerat dicta animi assumenda, dolorem nulla saepe, eligendi ab eveniet ipsum.</p>
+
+                        <div style={{display: "flex", justifyContent: "space-between"}}>
+                            <button className="btn btn-primary" onClick={subId}>Diminuir</button>
+                            <button className="btn btn-primary" onClick={addId}>Acrescentar</button>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </>
     )
 }
